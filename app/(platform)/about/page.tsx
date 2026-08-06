6ccbc8b84851ab5c086fc062c80243964e4b5c03
awesomeosep <99228798+awesomeosep@users.noreply.github.com>
2026-08-06 16:17:09 -0400

    updated about page

--- app/(platform)/about/page.tsx
+++ app/(platform)/about/page.tsx
@@ -7,7 +7,7 @@ export default function DashboardPage() {
         <div className="flex flex-col max-w-full gap-4">
           <div className="flex flex-col gap-4">
             <h1 className="text-3xl">About</h1>
-            <p>FriendlyFace is a web app platform to connect people with similar interest at lunch.</p>
+            <p>FriendlyFace is a web app platform to connect people at lunch.</p>
             <p>{"With technology and social media, young people are more disconnected in-person than ever. We recognize that it can be difficult to find and connect with like-minded individuals."}</p>
             <p>We strive to create an open, inclusive space where people can fnd each other and build meaningful connections.</p>
           </div>
