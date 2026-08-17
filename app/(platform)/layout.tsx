dcf3d326803e4ca86d04f0dd653f23b8458b587f
awesomeosep <99228798+awesomeosep@users.noreply.github.com>
2026-08-17 17:24:46 -0400

    added disclaimer and fixed Fit button in editor/viewer

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -3,7 +3,6 @@
 import NavBar from "@/components/NavBar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
 const queryClient = new QueryClient();
 
 export default function Layout({
@@ -14,10 +13,19 @@ export default function Layout({
   return (
     <div className="h-dvh w-screen flex flex-col z-0 relative overflow-hidden">
       <NavBar></NavBar>
-      <div className="flex flex-col w-screen min-h-full overflow-y-auto overflow-x-hidden">
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+      <div className="flex flex-col w-screen min-h-full max-h-full max-h-screen">
+        <div className="flex flex-col h-full max-h-full overflow-y-auto overflow-x-hidden">
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </div>
+        <div className="border-t border-black h-fit p-2 flex flex-row border-black">
+          <p className="text-xs text-muted-foreground">
+            Frindly Face is currently being piloted. Exercise judgement when finding a table. If you encounter issues or
+            any form of unacceptable behavior, report to your school counselor
+            or administrators.
+          </p>
+        </div>
       </div>
     </div>
   );
